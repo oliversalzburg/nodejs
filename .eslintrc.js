db40b789591ee6c0639852ac78321a19d41fa27b
Oliver Salzburg <oliver.salzburg@gmail.com>
2022-04-05 15:23:04 +0200

    fix: Ignore output

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,26 @@
-{
-  "root": true,
-  "env": {
-    "node": true,
-    "es6": true
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es6: true,
   },
-  "plugins": ["@typescript-eslint"],
-  "extends": [
+  plugins: ["@typescript-eslint"],
+  extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended"
+    "plugin:@typescript-eslint/recommended",
   ],
-  "rules": {
+  rules: {
     "@typescript-eslint/no-explicit-any": [
       "error",
       {
-        "ignoreRestArgs": true
-      }
+        ignoreRestArgs: true,
+      },
     ],
     "@typescript-eslint/no-unused-vars": ["off"],
     "@typescript-eslint/no-var-requires": ["off"],
     "no-unused-expressions": "warn",
-    "quotes": "warn"
-  }
-}
+    quotes: "warn",
+  },
+  ignorePatterns: ["output/*"],
+};
