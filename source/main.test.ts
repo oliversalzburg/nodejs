b76e84917a2fe273021ca5fe07f506b956a2d42c
Oliver Salzburg <oliver.salzburg@adesso.de>
2024-02-15 14:05:18 +0100

    feat: Add testing scaffold

---
+++ source/main.test.ts
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-expressions */
+import { expect } from "chai";
+import { describe, it } from "mocha";
+import { nothing } from "./main.js";
+
+describe("main", () => {
+  it("should export nothing", () => {
+    expect(nothing).to.be.undefined;
+  });
+});
