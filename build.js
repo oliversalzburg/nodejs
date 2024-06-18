c2748f55bae90b72354a2de30637521f54d6381d
Oliver Salzburg <oliver.salzburg@gmail.com>
2024-06-18 12:48:13 +0200

    feat: Use shared eslint-config

--- build.js
+++ build.js
@@ -1,12 +1,14 @@
+import { redirectErrorsToConsole } from "@oliversalzburg/js-utils";
 import esbuild from "esbuild";
 
 esbuild
   .build({
     bundle: true,
     entryPoints: ["./source/main.ts"],
+    external: ["os"],
     format: "esm",
     outfile: "./output/main.js",
     platform: "node",
-    target: "node18",
+    target: "node20",
   })
-  .catch(console.error);
+  .catch(redirectErrorsToConsole(console));
